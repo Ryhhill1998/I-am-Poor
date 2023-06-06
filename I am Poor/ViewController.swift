95c7f885390b6c04b15c9e09223154c937dd81c3
ryhhill1998 <ryhhill1998@outlook.com>
2023-06-06 21:47:00 +0100

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Ryan Henzell-Hill on 06/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
